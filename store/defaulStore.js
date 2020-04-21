c12ff5c4dd306944555e778c49812b1f693eaf93
Juan Gaspar Calvo <juangaspar.calvopuga@gmail.com>
2020-04-21 02:15:04 +0200

    Fixed some tests

--- store/defaulStore.js
+++ store/defaulStore.js
@@ -4,11 +4,11 @@ const defaultStore = {
       info1:
         'Hola, mi nombre es Juan Gaspar, soy programador y esta es mi web personal',
       info2:
-        'Tenía intención de hablar sobre mi experiencia laboral, todo lo que he hecho en estos últimos 10 años, pero me parece que es aburrido hacerlo, así que si estás interesado puedes echar un vistazo a mi <a href="https://www.linkedin.com/in/juan-gaspar-calvo-puga/" target="_blank">linkedin</a>',
+        'Tenía intención de hablar sobre mi experiencia laboral, todo lo que he hecho en estos últimos 11 años, pero me parece que es aburrido hacerlo, así que si estás interesado puedes echar un vistazo a mi <a href="https://www.linkedin.com/in/juan-gaspar-calvo-puga/" target="_blank">linkedin</a>',
       info3:
         'Actualmente trabajo como desarrollador fullstack js en <a href="https://we.letgo.com" target="_blank">letgo</a>. Si quieres ver parte de mi trabajo echa un vistazo a nuestra magnífica <a href="https://letgo.com" target="_blank">pwa</a>. (Más info <a href="https://developers.google.com/web/progressive-web-apps/" target="_blank">pwa</a>)',
       info4:
-        'Esta web está construida con <a href="https://reactjs.org" target="_blank">React</a>, y servida con http2 usando <a href="http://expressjs.com" target="_blank">Express</a> como servidor http y <a href="https://github.com/zeit/next.js" target="_blank">Next</a> para el ssr',
+        'Esta web está construida con <a href="https://reactjs.org" target="_blank">React</a> y <a href="https://github.com/zeit/next.js" target="_blank">Next</a> para el ssr',
       info5:
         'Obviamente es una PWA para que puedas instalarla y visitarla offline. ¿Por qué no?',
       info6:
@@ -19,11 +19,11 @@ const defaultStore = {
       info1:
         "Hi, my name is Juan Gaspar, I'm developer and this is my personal web",
       info2:
-        'I had intended to talk about my work experience, everything I\'ve done in the last 10 years, but I think it\'s boring to do so, so if you\'re interested you can check my <a href="https://www.linkedin.com/in/juan-gaspar-calvo-puga/" target="_blank">linkedin</a>',
+        'I had intended to talk about my work experience, everything I\'ve done in the last 11 years, but I think it\'s boring to do so, so if you\'re interested you can check my <a href="https://www.linkedin.com/in/juan-gaspar-calvo-puga/" target="_blank">linkedin</a>',
       info3:
         'I am currently working as fullstack js developer at <a href="https://we.letgo.com" target="_blank">letgo</a>. If you want to see part of my work, take a look at our great <a href="https://letgo.com" target="_blank">pwa</a>. (More info <a href="https://developers.google.com/web/progressive-web-apps/" target="_blank">pwa</a>)',
       info4:
-        'This website is built with <a href="https://reactjs.org" target="_blank">React</a>, and served with http2 using <a href="http://expressjs.com" target= "_blank">Express</a> as an http server and <a href="https://github.com/zeit/next.js" target="_blank">Next</a> for the ssr',
+        'This website is built with <a href="https://reactjs.org" target="_blank">React</a> and <a href="https://github.com/zeit/next.js" target="_blank">Next</a> for the ssr',
       info5:
         'Obviously it is a PWA so you can install it and visit it offline. Why not?',
       info6:
